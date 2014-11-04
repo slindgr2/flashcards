335cf218d07201420c44f5706814166913e3f03d
Brady Garvin <brady.j.garvin@gmail.com>
2014-11-03 20:44:15 -0600

    Added questions variable in a separate file.

---
+++ questions.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+	question: 'What is the answer to this first question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this second question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this third question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this fourth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this fifth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this sixth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this seventh question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this eighth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this ninth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+];
