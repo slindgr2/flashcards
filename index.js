eb86a3ebc62c4c122b15caa6c80212e13402462b
slindgr2 <slindgr2@nebrwesleyan.edu>
2014-11-13 09:52:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,22 @@
 var x = 0;
 function post_question() {
   document.getElementById('questions') .innerHTML = questions[x].question;
+  document.getElementById('button1') .removeEventListener('click', correct);
+  document.getElementById('button1') .removeEventListener('click', incorrect);
+  document.getElementById('button2') .removeEventListener('click', correct);
+  document.getElementById('button2') .removeEventListener('click', incorrect);
+  document.getElementById('button3') .removeEventListener('click', correct);
+  document.getElementById('button3') .removeEventListener('click', incorrect);
   document.getElementById('button1') .addEventListener('click', correct);
   document.getElementById('button2') .addEventListener('click', incorrect);
-  document.getElementById('button3') .addEventListener('click', incorrect);
+  document.getElementById('button3') .addEventListener('click', incorrect); 
   document.getElementById('button1') .innerHTML = questions[x].options[0];
   document.getElementById('button2') .innerHTML = questions[x].options[1];
   document.getElementById('button3') .innerHTML = questions[x].options[2];
   document.getElementById('button1') .disabled = false;
   document.getElementById('button2') .disabled = false;
   document.getElementById('button3') .disabled = false;
+  x = x + 1;
 }
 function correct() {
   document.getElementById('questions') .innerHTML = 'correct';
@@ -33,3 +40,4 @@ function wait() {
   window.setTimeout(post_question, 3000);
 }
 post_question();
+
